66256049f1bd91c49a1b8e86d219feb8197d2009
flora-m <floramolnar@hotmail.com>
2020-12-29 18:06:53 +0100

    updated generateData and comments

--- github_access.py
+++ github_access.py
@@ -1,9 +1,18 @@
 import tmp
 from github import Github
 
+# personal access token
 name = "flora-m"
 token = tmp.getToken()
 g = Github(token)
 user = g.get_user()
 
-def(generateData(repoAddress, graphData))
+
+def generateData(repoAddress, graphData):
+    # create child for repo
+    child = {
+        "name": repoAddress,
+        "children": getRepoData(repoAddress)
+    }
+    # append repo to overall data
+    graphData["children"].append(child)
