f654b7b5a97b4fc21ca612b90953c1bdace4b55f
flora-m <floramolnar@hotmail.com>
2020-12-30 13:31:43 +0100

    added main function and some other small changes

--- github_access.py
+++ github_access.py
@@ -1,8 +1,10 @@
-import tmp
+import tmp 
 from github import Github
+import datetime
+import json
 
 # personal access token
-name = "flora-m"
+# name = "flora-m"
 token = tmp.getToken()
 g = Github(token)
 user = g.get_user()
@@ -51,18 +53,35 @@ def getRepoData(repoAddress):
     ]
 
     repo = g.get_repo(repoAddress)
-
+    # get commits from repo (only master/main branch)
     commits = repo.get_commits()
     for commit in commits:
         if commit.commit is not None:
             weekday = (commit.commit.author.date).weekday()
-            author = commit.commmit.author
+            author = commit.commit.author.name
 
-            authors = [x for x in week[weekday]["children"] if x["name"] == (author)]
+            authors = [x for x in week[weekday]["children"] if x["name"] == str(author)]
             if(not authors):
-                week[weekday]["children"].append({"name": (author), "value": 1})
+                week[weekday]["children"].append({"name": str(author), "value": 1})
             else:
                 authorObject = authors[0]
                 authorObject["value"] += 1
     
     return week
+
+
+def main():
+    graphData = {
+        "name" : "data",
+        "children" : []
+    }
+    generateData("nating/cs-exams", graphData)
+    
+    # export data to data.json file
+    with open('data.json', 'w') as outfile:
+        json.dump(graphData, outfile)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
